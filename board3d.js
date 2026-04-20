c91b2467f86c474bd88544d5a842978d0fc0a647
Saima Zadran <zadran_s@auca.kg>
2026-04-21 00:15:49 +0200

    Adjust top pit numbers, bottom pit counts, and move kazan counts above

--- board3d.js
+++ board3d.js
@@ -352,7 +352,7 @@ for (let i = 0; i < 18; i++) {
     countSprite.position.set(
         base.x,
         1.34,
-        isTopRow ? base.z + 1.00 : base.z - 1.20
+        isTopRow ? base.z + 1.00 : base.z - 1.50
     );
     scene.add(countSprite);
     pitCountSprites[i] = countSprite;
